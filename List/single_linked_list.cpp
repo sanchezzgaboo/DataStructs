ee654940a7160384ad5e8ddb5b02eb1df4f1b199
SisasCamel <gsanchezv07@outlook.es>
2025-09-24 08:02:21 -0500

    init

---
+++ List/single_linked_list.cpp
@@ -0,0 +1,9 @@
+#include "single_linked_list.h"
+
+single_linked_list::single_linked_list(/* args */)
+{
+}
+
+single_linked_list::~single_linked_list()
+{
+}
---
+++ List/single_linked_list.h
@@ -0,0 +1,8 @@
+class single_linked_list
+{
+private:
+    int size;
+public:
+    single_linked_list(/* args */);
+    ~single_linked_list();
+};
